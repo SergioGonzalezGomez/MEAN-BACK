11681377e91303ef69a39b2a5b94589bd4ac271a
SergioGonzalezGomez <sergiogonzalezgomez15@gmail.com>
2022-04-04 18:47:14 +0200

    featIgnore

---
+++ controllers/productoController.js
@@ -0,0 +1,84 @@
+const Producto = require('../models/Producto');
+
+
+exports.crearProducto = async (req, res) => {
+    try {
+        let producto;
+        //Creamos el producto
+        producto = new Producto(req.body);
+        await producto.save();
+        res.send(producto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.obtenerProductos = async (req, res) => {
+    try {
+        const productos = await Producto.find();
+        res.json(productos);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.actualizarProducto = async (req, res) => {
+    try {
+        const { nombre, categoria, ubicacion, precio } = req.body;
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto) {
+            res.status(404).json({ msg: 'No existe el producto' });
+        }
+
+        producto.nombre = nombre;
+        producto.categoria = categoria;
+        producto.ubicacion = ubicacion;
+        producto.precio = precio;
+
+        producto = await Producto.findOneAndUpdate({ _id: req.params.id }, producto, {new: true})
+        res.json(producto)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.obtenerProducto = async (req, res) => {
+    try {
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto) {
+            res.status(404).json({ msg: 'No existe el producto' });
+        }
+
+        res.json(producto);
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.eliminarProducto = async (req, res) => {
+    try {
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto) {
+            res.status(404).json({ msg: 'No existe el producto' });
+        }
+
+        await Producto.findOneAndRemove({ _id: req.params.id })
+
+        res.json({ msg: 'Producto eliminado con éxito' });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+
+//Creamos el servidor
+const app = express();
+
+
+//Conectamos a la BD
+conectarDB();
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/productos', require('./routes/producto'))
+
+
+
+app.listen(4000, () => {
+    console.log('El servidor está corriendo perfectamente');
+})
